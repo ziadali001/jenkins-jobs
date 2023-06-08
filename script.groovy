b57a49915abe8c167ea6c47b4396e7700fb666c1
ziadali001 <106988846+ziadali001@users.noreply.github.com>
2023-06-08 14:23:16 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,17 +1,3 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t ziadmali/my-repo:jma-3.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push ziadmali/my-repo:jma-3.0'
-    }
-} 
-
 def deployApp() {
     echo 'deploying the application...'
 } 
